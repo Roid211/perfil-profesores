fb8233e3543b3bfe676c4cf561843c589f5be00e
Roid211 <david_rp21@hotmail.com>
2024-11-18 19:44:35 -0500

    Mensaje de commit

--- finalsistemas/next.config.ts
+++ finalsistemas/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true, // Activa el modo estricto de React
   images: {
     domains: ['utp.ac.pa', 'picsum.photos'],  // Agrega los dominios aquí
   },
